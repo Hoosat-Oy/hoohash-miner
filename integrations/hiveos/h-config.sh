92b702052386f8bb3be5e98084dda4d9f00196fe
Toni Lukkaroinen <toni.lukkaroinen@hoosat.fi>
2024-07-08 12:28:28 +0300

    Basics done for forking, now we can focus on implementing hoohash.

---
+++ integrations/hiveos/h-config.sh
@@ -0,0 +1,21 @@
+####################################################################################
+###
+### hoohash-miner
+### https://github.com/Hoosat-Oy/hoohash-miner/releases
+###
+### Hive integration: Merlin
+###
+####################################################################################
+
+#!/usr/bin/env bash
+[[ -e /hive/custom ]] && . /hive/custom/hoohash-miner/h-manifest.conf
+[[ -e /hive/miners/custom ]] && . /hive/miners/custom/hoohash-miner/h-manifest.conf
+conf=""
+conf+=" --hoohashd-address=$CUSTOM_URL --mining-address $CUSTOM_TEMPLATE"
+
+
+[[ ! -z $CUSTOM_USER_CONFIG ]] && conf+=" $CUSTOM_USER_CONFIG"
+
+echo "$conf"
+echo "$conf" > $CUSTOM_CONFIG_FILENAME
+
