29810185842d0f3e0db14cd78e32a7269e2d6b9d
Toni Lukkaroinen <toni.lukkaroinen@hoosat.fi>
2024-07-11 12:24:37 +0300

    Compute_30 and20 disabled

--- build-cuda.sh
+++ build-cuda.sh
@@ -6,6 +6,6 @@ nvcc plugins/cuda/hoohash-cuda-native/src/hoohash-cuda.cu -std=c++11 -O3 --restr
 # Compute version 6.1
 nvcc plugins/cuda/hoohash-cuda-native/src/hoohash-cuda.cu -std=c++11 -O3 --restrict --ptx --gpu-architecture=compute_61 --gpu-code=sm_61 -o plugins/cuda/resources/hoohash-cuda-sm61.ptx -Xptxas -O3 -Xcompiler -O3
 # Compute version 3.0
-nvcc plugins/cuda/hoohash-cuda-native/src/hoohash-cuda.cu -ccbin=gcc-7 -std=c++11 -O3 --restrict --ptx --gpu-architecture=compute_30 --gpu-code=sm_30 -o plugins/cuda/resources/hoohash-cuda-sm30.ptx
+# nvcc plugins/cuda/hoohash-cuda-native/src/hoohash-cuda.cu -ccbin=gcc-7 -std=c++11 -O3 --restrict --ptx --gpu-architecture=compute_30 --gpu-code=sm_30 -o plugins/cuda/resources/hoohash-cuda-sm30.ptx
 # Compute version 2.0
-nvcc plugins/cuda/hoohash-cuda-native/src/hoohash-cuda.cu -ccbin=gcc-5 -std=c++11 -O3 --restrict --ptx --gpu-architecture=compute_20 --gpu-code=sm_20 -o plugins/cuda/resources/hoohash-cuda-sm20.ptx
+# nvcc plugins/cuda/hoohash-cuda-native/src/hoohash-cuda.cu -ccbin=gcc-5 -std=c++11 -O3 --restrict --ptx --gpu-architecture=compute_20 --gpu-code=sm_20 -o plugins/cuda/resources/hoohash-cuda-sm20.ptx
